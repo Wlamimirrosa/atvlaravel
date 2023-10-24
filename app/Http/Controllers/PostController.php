0203169b1c0fda249c94af66e18688d16283d261
Wlamimirrosa <129913640+Wlamimirrosa@users.noreply.github.com>
2023-10-24 16:48:14 -0300

    Add files via upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return response()->json($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'titulo' => 'required',
+            'corpo' => 'required',
+        ]);
+
+        $post = Post::create($request->all());
+
+        return response()->json($post, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        if (is_null($post)) {
+            return response()->json(null, 404);
+        }
+
+        return response()->json($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::find($id);
+
+        if (is_null($post)) {
+            return response()->json(null, 404);
+        }
+
+        $post->update($request->all());
+
+        return response()->json($post, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+
+        if (is_null($post)) {
+            return response()->json(null, 404);
+        }
+
+        $post->delete();
+
+        return response()->json(null, 204);
+    }
+}
